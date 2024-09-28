746ecf1202f753685ed5e60c744c032687576736
ilhamelgharbaoui <ielgharbaoui@azura-group.com>
2024-09-28 19:36:23 +0100

    Code refactoring

--- src/components/todo-items/CardItems.jsx
+++ src/components/todo-items/CardItems.jsx
@@ -1,4 +1,4 @@
-import TodoItem from "./TodoItem.jsx";
+import {TrashIcon} from "@heroicons/react/24/outline/index.js";
 
 const CardItems = ({items, onDeleteItem}) => {
   const isEmpty = items.length === 0
@@ -11,7 +11,16 @@ const CardItems = ({items, onDeleteItem}) => {
     <ul>
       { !isEmpty &&
         items.map((item) => (
-          <TodoItem item={item} key={item.id} onDelete={handleDeleteItem} />
+          <li
+            key={item.id}
+            className={'flex items-center justify-between bg-gray-800 rounded p-6 mb-3'}
+            draggable={true}
+          >
+            <span>{ item.title }</span>
+            <button type={'button'} onClick={() => {handleDeleteItem(item.id)}}>
+              <TrashIcon className={'w-6 text-rose-400'} />
+            </button>
+          </li>
         ))
       }
     </ul>
